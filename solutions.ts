0c5d96cb1042d7d92e96095b19bc89f3143fa460
mnaofficialbd <mna.official92@gmail.com>
2025-11-18 11:35:36 +0600

    filterActiveUsers function problem solved

--- solutions.ts
+++ solutions.ts
@@ -25,19 +25,32 @@ const getLength = (value: string | any[]): number => {
 
 /* 3. problem */
 /* 4. filterByRating function problem */
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
 type itemsType = Array<{ title: string; rating: number }>;
 
 const filterByRating = (books: itemsType): itemsType => {
   return books.filter((item) => item.rating >= 4);
 };
-console.log(filterByRating(books));
 
 /* 5. filterActiveUsers function problem */
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
+];
+
+type userType = Array<{
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}>;
+
+const filterActiveUsers = (users: userType): userType => {
+  return users.filter((user) => user.isActive === true);
+};
+
+console.log(filterActiveUsers(users));
+
 /* 6. printBookDetails function problem */
 /* 7. getUniqueValues function problem */
 /* 8. calculateTotalPrice function problem */
