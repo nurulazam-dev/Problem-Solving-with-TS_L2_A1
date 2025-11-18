5cc207e455ade1b38868a0c10ad055028b8848d3
mnaofficialbd <mna.official92@gmail.com>
2025-11-18 09:59:22 +0600

    formatValue function problem solved

---
+++ solutions.ts
@@ -0,0 +1,9 @@
+const formatValue = (input: string | number | boolean) => {
+  if (typeof input === "string") {
+    return input.toUpperCase();
+  } else if (typeof input === "number") {
+    return input * 10;
+  } else if (typeof input === "boolean") {
+    return !input;
+  }
+};
