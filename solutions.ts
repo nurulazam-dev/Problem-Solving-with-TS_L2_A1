8cb1bcc4583aa2d579031b4494c3108204ded84b
mnaofficialbd <mna.official92@gmail.com>
2025-11-18 10:55:07 +0600

    getLength function problem solved

--- solutions.ts
+++ solutions.ts
@@ -1,3 +1,4 @@
+/* 1. formatValue function problem */
 const formatValue = (
   input: string | number | boolean
 ): string | number | boolean => {
@@ -10,6 +11,23 @@ const formatValue = (
   }
   return input;
 };
-console.log(formatValue("hello"));
-console.log(formatValue(5));
-console.log(formatValue(true));
+
+/* 2. getLength function problem */
+const getLength = (value: string | any[]): number => {
+  if (Array.isArray(value)) {
+    return value.length;
+  } else if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value;
+  }
+};
+console.log(getLength("This is a test"));
+console.log(getLength([10, 20, 30, 40]));
+
+/* 3. problem */
+/* 4. filterByRating function problem */
+/* 5. filterActiveUsers function problem */
+/* 6. printBookDetails function problem */
+/* 7. getUniqueValues function problem */
+/* 8. calculateTotalPrice function problem */
